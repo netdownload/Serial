643b87a9ae15c6c3176f561f00b19d0ffc3bdbc9
netdownload <netdownload@yandex.ru>
2020-05-05 13:27:41 +0300

    some improvments

--- spider_gas.py
+++ spider_gas.py
@@ -1,3 +1,8 @@
+# TODO Возникают ситуации, что при считывании данных из прибора не находится почасовой срез данных
+#      Необходимо определить и записать в базу ближайщие  по времени значения +1 секунда
+# TODO Запись результата работы в лог
+# TODO Вывод сообщений в дискорд/телеграм/whatsup/viber
+
 # Библитотека для работы с COM портом
 import serial
 import time
@@ -6,6 +11,7 @@ import datetime
 import libscrc
 # Библиотека для работы с базой данных MySQL
 import pymysql
+import re
 
 INIT_PORT = b'\x2F\x3F\x21\x0D\x0A'
 OPEN_DEVICE = b'\x06\x30\x36\x31\x0D\x0A'
@@ -13,8 +19,9 @@ CLOSE_DEVICE = b'\x01\x42\x30\x03\x71\x01\x42\x30\x03\x71'
 DEVICE_NAME = b'\x2f\x45\x6c\x73\x36\x45\x4b\x32\x36\x30\x0d\x0a'
 BEGIN_REQ = b'\x01\x52\x33\x02\x33\x3a\x56\x2e\x30\x28\x33\x3b'
 END_REQ = b'1)' + b'\x03'
-DELAY = 1
+DELAY = 5
 NUMBER_OF_ATTEMPTS = 10
+CRC_OK = 'CRC Ok'
 
 
 def delta_hours(date_end, date_begin):
@@ -61,6 +68,22 @@ def insert_values_into_database():  # TODO Insert function
     pass
 
 
+def check_crc_and_date(answer_for_check_crc):               # TODO Дабавить проверку по дате
+    check = split_answer_into_values(answer_for_check_crc)
+    if check[-2] == CRC_OK:
+        print(CRC_OK)
+    else:
+        print('Error')
+    return 0
+
+
+def split_answer_into_values(answer):
+    values = re.split(r'\)\(|\(|\)', answer.decode())
+    # values.remove('')
+    # values.pop()
+    return values
+
+
 # crc16 = libscrc.modbus(INIT_PORT)
 # print(hex(crc16)[1::2] + hex(crc16)[2::2])  # TODO разобраться в срезах
 
@@ -99,7 +122,7 @@ while not is_read_ok and NUMBER_OF_ATTEMPTS > 0:
             crc = calculate_crc(request_string)
             ser.write(request_string + crc)
             time.sleep(DELAY)
-            print(ser.readall())
+            check_crc_and_date((ser.readall()))
             insert_values_into_database()
         ser.write(CLOSE_DEVICE)
         time.sleep(DELAY)
