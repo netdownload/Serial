521b33ac69777b02ed1b049f1ed31cbd39244ba2
netdownload <netdownload@yandex.ru>
2020-05-15 13:21:55 +0300

    add demo file with creation xlsx file for report

---
+++ Electro/report_electro.py
@@ -0,0 +1,24 @@
+# pip install XlsxWriter
+import xlsxwriter
+
+workbook = xlsxwriter.Workbook('demo.xlsx')
+worksheet = workbook.add_worksheet()
+
+# Widen the first column to make the text clearer.
+worksheet.set_column('A:A', 20)
+
+# Add a bold format to use to highlight cells.
+bold = workbook.add_format({'bold': True})
+
+# Write some simple text.
+worksheet.write('A1', 'Hello')
+
+# Text with formatting.
+worksheet.write('A2', 'World', bold)
+
+# Write some numbers, with row/column notation.
+worksheet.write(2, 0, 123, bold)
+worksheet.write(3, 0, 123.456)
+
+
+workbook.close()
