426f1f326142853d840d61aaf6a0010b1edba925
netdownload <netdownload@yandex.ru>
2020-05-29 13:20:16 +0300

    add send report by e-mail

--- Electro/report_electro.py
+++ Electro/report_electro.py
@@ -13,11 +13,14 @@ import sys
 import argparse
 from decimal import Decimal
 import smtplib
-import email
-from email import encoders
-from email.mime.base import MIMEBase
+import mimetypes  # Импорт класса для обработки неизвестных MIME-типов, базирующихся на расширении файла
+from email import encoders  # Импортируем энкодер
+from email.mime.base import MIMEBase  # Общий тип
+from email.mime.text import MIMEText  # Текст/HTML
+from email.mime.image import MIMEImage  # Изображения
+from email.mime.audio import MIMEAudio  # Аудио
 from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+import os
 
 COMPANY = 'ОАО "Аньковское"'
 ENGINEER = 'Главный энергетик_________________'
@@ -403,6 +406,7 @@ def delta_period(date_end, date_begin):
     else:
         return 0
 
+
 # Запуск скрипта с параметрами
 # https://jenyay.net/Programming/Argparse
 def create_parser():
@@ -469,7 +473,9 @@ answer56 = get_power_values_from_database56(date_time_begin_obj, date_time_end_o
 
 delta = delta_period(date_time_end_obj, date_time_begin_obj)
 
-workbook = xlsxwriter.Workbook('demo.xlsx')
+filename = 'ЭЭ_' + month_txt + '_' + str(year_txt) + '.xlsx'
+
+workbook = xlsxwriter.Workbook(filename)
 worksheet = workbook.add_worksheet()
 worksheet.name = 'Worksheet'
 
@@ -1170,22 +1176,76 @@ worksheet4.write('A40', 'Исполнитель:______________________________',
 worksheet4.merge_range('I40:L40', '______________________________ /дата/', format_center_times_10)
 workbook.close()
 
+
 # Отправка отчета по почте
-HOST = "mail.tm-ankovo.ru"
-SUBJECT = "Test email from Python"
-TO = "sa@tm-ankovo.ru"
-FROM = "sa@tm-ankovo.ru"
-text = "Python 3.4 rules them all!"
+def send_email(addr_to, msg_subj, msg_text, files):
+    addr_from = 'sa@tm-ankovo.ru'  # Отправитель
+    password = 'password'  # Пароль
 
-BODY = "\r\n".join((
-    "From: %s" % FROM,
-    "To: %s" % TO,
-    "Subject: %s" % SUBJECT,
-    "",
-    text
-))
+    msg = MIMEMultipart()  # Создаем сообщение
+    msg['From'] = addr_from  # Адресат
+    msg['To'] = addr_to  # Получатель
+    msg['Subject'] = msg_subj  # Тема сообщения
 
-server = smtplib.SMTP(HOST)
-server.login('sa@tm-ankovo.ru', 'password')
-server.sendmail(FROM, [TO], BODY)
-server.quit()
+    body = msg_text  # Текст сообщения
+    msg.attach(MIMEText(body, 'plain'))  # Добавляем в сообщение текст
+
+    process_attachement(msg, files)
+
+    # -------- Этот блок настраивается для каждого почтового провайдера отдельно
+    server = smtplib.SMTP('mail.tm-ankovo.ru', 587)
+    # server = smtplib.SMTP_SSL('smtp.server.ru', 465) # Создаем объект SMTP
+    server.starttls()  # Начинаем шифрованный обмен по TLS
+    # server.set_debuglevel(True)                      # Включаем режим отладки, если не нужен - можно закомментировать
+    server.login(addr_from, password)  # Получаем доступ
+    server.send_message(msg)  # Отправляем сообщение
+    server.quit()  # Выходим
+
+
+# --------------------------
+
+
+def process_attachement(msg, files):  # Функция по обработке списка, добавляемых к сообщению файлов
+    for f in files:
+        if os.path.isfile(f):  # Если файл существует
+            attach_file(msg, f)  # Добавляем файл к сообщению
+        elif os.path.exists(f):  # Если путь не файл и существует, значит - папка
+            dir = os.listdir(f)  # Получаем список файлов в папке
+            for file in dir:  # Перебираем все файлы и...
+                attach_file(msg, f + "/" + file)  # ...добавляем каждый файл к сообщению
+
+
+def attach_file(msg, filepath):  # Функция по добавлению конкретного файла к сообщению
+    filename = os.path.basename(filepath)  # Получаем только имя файла
+    ctype, encoding = mimetypes.guess_type(filepath)  # Определяем тип файла на основе его расширения
+    if ctype is None or encoding is not None:  # Если тип файла не определяется
+        ctype = 'application/octet-stream'  # Будем использовать общий тип
+    maintype, subtype = ctype.split('/', 1)  # Получаем тип и подтип
+    if maintype == 'text':  # Если текстовый файл
+        with open(filepath) as fp:  # Открываем файл для чтения
+            file = MIMEText(fp.read(), _subtype=subtype)  # Используем тип MIMEText
+            fp.close()  # После использования файл обязательно нужно закрыть
+    elif maintype == 'image':  # Если изображение
+        with open(filepath, 'rb') as fp:
+            file = MIMEImage(fp.read(), _subtype=subtype)
+            fp.close()
+    elif maintype == 'audio':  # Если аудио
+        with open(filepath, 'rb') as fp:
+            file = MIMEAudio(fp.read(), _subtype=subtype)
+            fp.close()
+    else:  # Неизвестный тип файла
+        with open(filepath, 'rb') as fp:
+            file = MIMEBase(maintype, subtype)  # Используем общий MIME-тип
+            file.set_payload(fp.read())  # Добавляем содержимое общего типа (полезную нагрузку)
+            fp.close()
+            encoders.encode_base64(file)  # Содержимое должно кодироваться как Base64
+    file.add_header('Content-Disposition', 'attachment', filename=filename)  # Добавляем заголовки
+    msg.attach(file)
+
+
+# Использование функции send_email()
+addr_to = "sa@tm-ankovo.ru"  # Получатель
+files = ['./' + filename]  # Если нужно отправить все файлы из заданной папки, нужно указать её
+
+send_email(addr_to, 'Отчет по электроэнергии за ' + month_txt + ' ' + str(year_txt),
+           'Отчет по электроэнергии за ' + month_txt + ' ' + str(year_txt), files)
